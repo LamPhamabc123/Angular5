aa97144ae1373fe0eb83074c359287c7f4e1c345
Lam Pham <lpham27@csc.com>
2018-11-09 14:29:48 +0700

    Using types

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -14,22 +14,51 @@ import { Component } from '@angular/core';
 
 export class UserComponent {
     // Properties
-    firstName = 'Lam';
-    lastName = 'Pham';
-    age = 30;
-    address = {
-        street: '54 Le Van Huan',
-        district: 'Tan Binh',
-        city: 'HCM'
-    }
+    firstName: string;
+    lastName: string;
+    age: number;
+    address;
+
+    foo: any;
+    hasKids: boolean;
+    numberArray: number[];
+    stringArray: string[];
+    mixedArray: any[];
+    myTuple: [string, number, boolean];
+    unusable: void;
+    u: undefined;
+    n: null;
+
 
     // Methods
     constructor() {
-        
+        this.firstName = 'Lam';
+        this.lastName = 'Pham';
+        this.age = 30;
+
+        this.address = {
+            street: '54 Le Van huan',
+            city: 'HCM',
+            district: 'Tan Binh'
+        }
+
+        this.foo = 1;
+        this.hasKids = true;
+        this.numberArray = [1,2,3,4];
+        this.stringArray = ['hello', 'world'];
+        this.mixedArray =[true, undefined, 'hello'];
+        this.myTuple =['hello', 1, true];
+        this.unusable = undefined;
+        this.u = undefined;
+        this.n = null;
     }
 
     showAge() {
         return this.age + 2;
+    }
+
+    addNumbers(num1: number, num2: number) {
+        return num1 + num2;
     }
 
 }
