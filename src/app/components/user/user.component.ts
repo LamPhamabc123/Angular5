5cff7f1162408fa87efca4c1b4035ea6789efecb
Lam Pham <lpham27@csc.com>
2018-11-09 15:04:44 +0700

    Cleanup and interfaces

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import { User } from '../../models/User';
+
 @Component({
     selector: 'app-user',
     templateUrl: './user.component.html',
@@ -14,52 +16,21 @@ import { Component } from '@angular/core';
 
 export class UserComponent {
     // Properties
-    firstName: string;
-    lastName: string;
-    age: number;
-    address;
-
-    foo: any;
-    hasKids: boolean;
-    numberArray: number[];
-    stringArray: string[];
-    mixedArray: any[];
-    myTuple: [string, number, boolean];
-    unusable: void;
-    u: undefined;
-    n: null;
-
+    user: User;
 
     // Methods
     constructor() {
-        this.firstName = 'Lam';
-        this.lastName = 'Pham';
-        this.age = 30;
+        this.user = {
+            firstName: 'Lam',
+            lastName: 'Pham',
+            age: 30,
 
-        this.address = {
-            street: '54 Le Van huan',
-            city: 'HCM',
-            district: 'Tan Binh'
+            address: {
+                street: '54 Le Van Huan',
+                city: 'HCM',
+                district: 'Tan Binh'
+            }
         }
-
-        this.foo = 1;
-        this.hasKids = true;
-        this.numberArray = [1,2,3,4];
-        this.stringArray = ['hello', 'world'];
-        this.mixedArray =[true, undefined, 'hello'];
-        this.myTuple =['hello', 1, true];
-        this.unusable = undefined;
-        this.u = undefined;
-        this.n = null;
-        
-    }
-
-    showAge() {
-        return this.age + 2;
-    }
-
-    addNumbers(num1: number, num2: number): number {
-        return num1 + num2;
-    }
-
+   }
 }
+
