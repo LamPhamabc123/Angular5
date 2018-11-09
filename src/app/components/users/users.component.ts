f66b73ed47ecee87ec75bcc4d770082836d76387
Lam Pham <lpham27@csc.com>
2018-11-09 15:58:03 +0700

    bngFor ngIf

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../../models/User';
 
 @Component({
   selector: 'app-users',
@@ -6,10 +7,66 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./users.component.css']
 })
 export class UsersComponent implements OnInit {
+  users: User[];
 
-  constructor() { }
+  constructor() {
+    // console.log('contructor...');
+  }
 
   ngOnInit() {
+    // console.log('init...');
+    this.users = [
+      {
+        firstName: 'Lam',
+        lastName: 'Pham',
+        age: 32,
+
+        address: {
+          street: '54 Le Van Huan',
+          city: 'HCM',
+          district: 'Tan Binh'
+        }
+      },
+      {
+        firstName: 'Nam',
+        lastName: 'Pham',
+        age: 1,
+
+        address: {
+          street: '55 Le Van Huan',
+          city: 'HCM',
+          district: 'Tan Binh'
+        }
+      },
+      {
+        firstName: 'An',
+        lastName: 'Pham',
+        age: 1,
+
+        address: {
+          street: '56 Le Van Huan',
+          city: 'HCM',
+          district: 'Tan Binh'
+        }
+      }
+    ]
+
+    this.addUser(
+      {
+        firstName: 'Duyen',
+        lastName: 'Gia',
+        age: 30,
+
+        address: {
+          street: '54 Le Van Huan',
+          city: 'HCM',
+          district: 'Tan Binh'
+        }
+      });
+  }
+
+  addUser(user: User) {
+    this.users.push(user)
   }
 
 }
